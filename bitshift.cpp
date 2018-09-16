1af4ef3f595d40692618afb09a7691942be56faa
krook1024 <krook1024@gmail.com>
2018-09-16 14:53:21 +0200

    Bitshift

---
+++ bitshift.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main () {
+	int a = 1, count = 0;
+
+
+	while(a != 0)
+	{
+		a <<= 1;
+		cout << a << endl;
+		count ++;
+	}
+
+	cout << "\nAz a " << count << " lépés után lesz hamis.\n";
+
+	return 0;
+}
