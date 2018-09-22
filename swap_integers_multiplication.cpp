b9c5ec7e42d542730c7d444d22115c56b2c0ad44
krook1024 <krook1024@gmail.com>
2018-09-22 21:10:22 +0200

    Add swap integers using the multiplication method

---
+++ swap_integers_multiplication.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+	// Swap integers using multiplication
+	int a = 7;
+	int b = 1;
+
+	a = a*b;	
+	b = a/b;		
+	a = a/b;
+
+	cout << a << " " << b << "\n";
+
+	return 0;
+}
