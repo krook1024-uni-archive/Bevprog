f35bdb60800290664fc29c7308c0792c517a9cb1
krook1024 <krook1024@gmail.com>
2018-10-19 21:42:00 +0200

    Add t.c párhuzamosítás, modósítás

--- xortores/crack.c
+++ xortores/crack.c
@@ -17,48 +17,37 @@ int main (void) {
 	char titkos[MAX_TITKOS];
 	char *p = titkos;
 	char *kulcs;
-	int olvasott_bajtok;
 
 	// titkos fajt berantasa
-	while ((olvasott_bajtok = read (0, (void *) p, (p - titkos + OLVASAS_BUFFER < MAX_TITKOS) ? OLVASAS_BUFFER : titkos + MAX_TITKOS - p))) {
-		p += olvasott_bajtok;
-	}
+	int olvasott_bajtok;
+	while ((olvasott_bajtok = read (0, (void *) p, (p - titkos + OLVASAS_BUFFER < MAX_TITKOS) ? OLVASAS_BUFFER : titkos + MAX_TITKOS - p))) p += olvasott_bajtok;
 
 	// maradek hely nullazasa a titkos bufferben
-	for (int i = 0; i < MAX_TITKOS - (p - titkos); ++i) {
-		titkos[p - titkos + i] = '\0';
-	}
-
-	if ((kulcs = (char *)malloc(sizeof(char)*KULCS_MERET)) == NULL)
-	{
-		printf("Memoria (kulcs) faliora\n");
-		exit(-1);
-	}
+	for (int i = 0; i < MAX_TITKOS - (p - titkos); ++i) titkos[p - titkos + i] = '\0';
 
 	int ii, ki, ji, li, mi, ni, oi, pi;
-#pragma omp paralell for private(kulcs, ii, ki, ji, li, mi, ni, oi, pi) shared(p, titkos)
+
+	#pragma omp parallel for private(kulcs, ii, ki, ji, li, mi, ni, oi, pi) shared(p, titkos)
 	// osszes kulcs eloallitasa
 	for (ii = '0'; ii <= '9'; ++ii)
-		for (ji = '0'; ji <= '9'; ++ji)
-			for (ki = '0'; ki <= '9'; ++ki)
-				for (li = '0'; li <= '9'; ++li)
-					for (mi = '0'; mi <= '9'; ++mi)
-						for (ni = '0'; ni <= '9'; ++ni)
-							for (oi = '0'; oi <= '9'; ++oi)
-								for (pi = '0'; pi <= '9'; ++pi) {
-									printf("%p\n", kulcs);
+	for (ji = '0'; ji <= '9'; ++ji)
+	for (ki = '0'; ki <= '9'; ++ki)
+	for (li = '0'; li <= '9'; ++li)
+	for (mi = '0'; mi <= '9'; ++mi)
+	for (ni = '0'; ni <= '9'; ++ni)
+	for (oi = '0'; oi <= '9'; ++oi)
+	for (pi = '0'; pi <= '9'; ++pi) {
+		if ((kulcs = (char *)malloc(sizeof(char)*KULCS_MERET)) == NULL) {
+			printf("Memoria (kulcs) faliora\n");
+			exit(-1);
+		}
 
-									kulcs[0] = ii;
-									kulcs[1] = ji;
-									kulcs[2] = ki;
-									kulcs[3] = li;
-									kulcs[4] = mi;
-									kulcs[5] = ni;
-									kulcs[6] = oi;
-									kulcs[7] = pi;
+		kulcs[0] = ii; kulcs[1] = ji; kulcs[2] = ki;
+		kulcs[3] = li; kulcs[4] = mi; kulcs[5] = ni;
+		kulcs[6] = oi; kulcs[7] = pi;
 
-									exor_tores(kulcs, KULCS_MERET, titkos, p - titkos);
-								}
+		exor_tores(kulcs, KULCS_MERET, titkos, p - titkos);
+	}
 
 	return 0;
 }
@@ -81,10 +70,10 @@ int tiszta_lehet (const char *titkos, int titkos_meret) {
 
 	double szohossz = atlagos_szohossz (titkos, titkos_meret);
 
-	return szohossz > 6.0 && szohossz < 9.0
-	&& strcasestr (titkos, "hogy") && strcasestr (titkos, "nem")
-	&& strcasestr (titkos, "és") && strcasestr (titkos, "mit")
-	&& strcasestr (titkos, "az") && strcasestr (titkos, "ha");
+
+	return szohossz > 3.0 && szohossz < 9.0
+		&& strcasestr(titkos, "hogy") && strcasestr(titkos, "nem") && strcasestr(titkos, "ne")
+		&& strcasestr(titkos, "az") && strcasestr(titkos, "ha");
 }
 
 void exor (const char kulcs[], int kulcs_meret, char titkos[], int titkos_meret, char *buffer) {
@@ -105,9 +94,12 @@ void exor_tores (const char kulcs[], int kulcs_meret, char titkos[], int titkos_
 
 	exor (kulcs, kulcs_meret, titkos, titkos_meret, buffer);
 
-	if(tiszta_lehet (buffer, titkos_meret)) {
+	if(tiszta_lehet (buffer, titkos_meret))  {
 		printf("Kulcs: [%c%c%c%c%c%c%c%c]\nTiszta szoveg: [%s]\n",
-		kulcs[0],kulcs[1],kulcs[2],kulcs[3],kulcs[4],kulcs[5],kulcs[6],kulcs[7], buffer);
+			kulcs[0],kulcs[1],kulcs[2],kulcs[3],
+			kulcs[4],kulcs[5],kulcs[6],kulcs[7],
+			buffer
+		);
 	}
 
 	free(buffer);
